6b7f8a06edbe0ed890de2f04996558b110a2d895
Jean-Sébastien Bour <jsb@zenexity.fr>
2011-03-10 15:59:34 +0100

    Refactor UsersEvent : make it a wrapper around UserAction to broadcast them via Spring event framework Replace @IsIndividual and @IsHomeFolder with @IsUser Send fake HFMR to interested external services on each modification is it is valid

--- Model/src/java/fr/cg95/cvq/service/users/external/IExternalHomeFolderService.java
+++ Model/src/java/fr/cg95/cvq/service/users/external/IExternalHomeFolderService.java
@@ -6,19 +6,18 @@ import fr.cg95.cvq.business.payment.external.ExternalApplication;
 import fr.cg95.cvq.business.payment.external.ExternalHomeFolder;
 import fr.cg95.cvq.business.users.external.HomeFolderMapping;
 import fr.cg95.cvq.exception.CvqModelException;
-import fr.cg95.cvq.security.annotation.IsHomeFolder;
-import fr.cg95.cvq.security.annotation.IsIndividual;
+import fr.cg95.cvq.security.annotation.IsUser;
 
 public interface IExternalHomeFolderService {
 
-    List<HomeFolderMapping> getHomeFolderMappings(@IsHomeFolder Long homeFolderId);
+    List<HomeFolderMapping> getHomeFolderMappings(@IsUser Long homeFolderId);
 
     void createHomeFolderMapping(HomeFolderMapping homeFolderMapping) throws CvqModelException;
 
     void modifyHomeFolderMapping(HomeFolderMapping homeFolderMapping) throws CvqModelException;
 
     HomeFolderMapping getHomeFolderMapping(final String externalServiceLabel,
-            @IsHomeFolder final Long homeFolderId);
+        @IsUser final Long homeFolderId);
 
     HomeFolderMapping getHomeFolderMapping(final String externalServiceLabel, 
             final String externalCapdematId);
@@ -35,7 +34,7 @@ public interface IExternalHomeFolderService {
      * its external id will be replaced by the given one.
      */
     void addHomeFolderMapping(final String externalServiceLabel,
-            @IsHomeFolder final Long homeFolderId, final String externalId);
+        @IsUser final Long homeFolderId, final String externalId);
 
     void deleteHomeFolderMapping(final String externalServiceLabel, ExternalHomeFolder eh);
 
@@ -43,7 +42,7 @@ public interface IExternalHomeFolderService {
      * Delete mappings for the given external service and home folder (included individual mappings).
      */
     void deleteHomeFolderMappings(final String externalServiceLabel,
-            @IsHomeFolder final Long homeFolderId);
+        @IsUser final Long homeFolderId);
 
     /**
      * Set the external id of an individual for the given external service.
@@ -52,6 +51,6 @@ public interface IExternalHomeFolderService {
      * To be used on external id retrieval from the external service.
      */
     void setExternalId(String externalServiceLabel,
-        @IsHomeFolder Long homeFolderId, @IsIndividual Long individualId,
+        @IsUser Long homeFolderId, @IsUser Long individualId,
         String externalId);
 }
